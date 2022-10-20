733904d561c0e15545a3914e3a5156ede861b9a1
SselJay <russeljaybuan@gmail.com>
2022-10-20 19:17:31 +0800

    added edit and update api + controller

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -32,6 +32,28 @@ class ClientController extends Controller
         ]);
     }
 
+    public function editClient ($id){
+        $client = Client::find($id);
+        return response()->json([
+            'status'=>200,
+            'client'=>$client,
+        ]);
+    }
+
+    public function updateClient (Request $request, $id){
+        $client = Client::find($id);
+        $client->name = $request->input('name');
+        $client->course = $request->input('course');
+        $client->email = $request->input('email');
+        $client->phone = $request->input('phone');
+        $client->update();
+
+        return response()->json([
+            'status' => 200,
+            'message'=>'Client Updated Successfully'
+        ]);
+    }
+
     public function deleteClient ($id){
         $client = Client::find($id);
         $client ->delete();
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/clients', [ClientController::class, 'getClients']);
 Route::post('/add-client', [ClientController::class, 'addClient']);
+Route::get('/edit-client/{id}', [ClientController::class, 'editClient']);
+Route::put('/update-client/{id}', [ClientController::class, 'updateClient']);
 Route::delete('/delete-client/{id}', [ClientController::class, 'deleteClient']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
